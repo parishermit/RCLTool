9e89ee85d6c6a43ea795a939ddd62726c47aaadb
panzhongjie <1275389939@qq.com>
2023-12-26 07:57:48 +0800

    Update api. Ref #15.

--- backend/trace_app/trace_api.py
+++ backend/trace_app/trace_api.py
@@ -1,16 +1,6 @@
-from datetime import datetime, timedelta
-import re
-import pytz
-import pandas as pd
-import time
-import json
 import sys
 import pathlib
-import os
-import csv
-import pymysql
 from trace_app.models import Span
-
 
 trace_app_path = pathlib.Path(__file__).parent
 sys.path.append(trace_app_path)
@@ -21,8 +11,7 @@ def get_span_list(trace_id):
     print(list(span_list))
     return list(span_list)
 
+
 def get_trace_list():
     trace_list = Span.objects.values('trace_id')
-    return list(set(map(lambda x:x['trace_id'],trace_list)))
-
-
+    return list(set(map(lambda x: x['trace_id'], trace_list)))
